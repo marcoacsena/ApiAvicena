723c207db5459e1cdf051a8543bf6dcb4f0d91ae
marcoacsena <marcoacsenagmail.com>
2019-10-28 15:12:05 -0300

    Initial commit

---
+++ app/src/main/java/br/com/marcoapps/apiavicena/view/MainActivity.java
@@ -0,0 +1,16 @@
+package br.com.marcoapps.apiavicena.view;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import br.com.marcoapps.apiavicena.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
