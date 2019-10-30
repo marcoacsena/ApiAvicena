e57899915cba1fa80fad560810890ebdd03e1766
lucianebenetti <LuluzinhaB2015>
2019-10-30 13:54:50 -0300

    commit Helper e MYORMLite

---
+++ app/src/main/java/br/com/marcoapps/apiavicena/dao/helpers/MyORMLiteHelper.java
@@ -0,0 +1,44 @@
+package br.com.marcoapps.apiavicena.dao.helpers;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+
+import com.j256.ormlite.android.apptools.OrmLiteSqliteOpenHelper;
+import com.j256.ormlite.support.ConnectionSource;
+import com.j256.ormlite.table.TableUtils;
+
+import java.sql.SQLException;
+
+import br.com.marcoapps.apiavicena.model.Medico;
+
+public class MyORMLiteHelper extends OrmLiteSqliteOpenHelper {
+    private static final String DATABASE_NAME = "avicena";
+    private static final int DATABASE_VERSION = 1;
+
+    public MyORMLiteHelper(Context c) {
+        super(c, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase, ConnectionSource connectionSource) {
+        try {
+            TableUtils.createTable(connectionSource, Medico.class);
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, ConnectionSource connectionSource, int i, int i1) {
+
+        try {
+            TableUtils.dropTable(connectionSource, Medico.class, true);
+
+            onCreate(sqLiteDatabase, connectionSource);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
